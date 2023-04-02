9baee208baf11b358ffc1b6675db3fdf87a7ccae
Gerson Santos <62805855+GersonESS@users.noreply.github.com>
2023-04-02 04:43:39 -0300

    a

---
+++ Aula02/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Aula02 POO PHP</title>
+</head>
+<body>
+    <?php 
+        require_once 'Caneta.php';
+        $c1 = new Caneta;
+        $c1->cor = "Azul";
+        $c1->ponta = 0.5;
+        $c1->tampada = false;
+        var_dump($c1);
+    ?>
+</body>
+</html>
